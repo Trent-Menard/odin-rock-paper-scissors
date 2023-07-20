f93556a44ee3dc02d79e3082af239b406c51f321
Trent Menard <trentmenard6@yahoo.com>
2023-07-20 17:41:36 -0500

    Merge to master

--- GameHandler.js
+++ GameHandler.js
@@ -94,6 +94,22 @@ function animateGameText(text, textStartIdx, textEndIdx, targetElement, miliSecD
     }, miliSecDelay);
 }
 
+const gameText = "I want to play a game.";
+
+for (i = 1; i <=3; i++){
+    const gameTextElement = document.createElement("h1");
+    gameTextElement.setAttribute("id", "GameText" + i);
+    const gameTextElementData = document.getElementById("GameText" + i);
+
+    // setTimeout(() => {
+    //     for (j = 0; j <=5; j++){
+    //         console.log(j);
+    //     }
+    // }, 5000);
+
+    document.body.append(gameTextElement);
+}
+
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
     // Return random Int [0-3] corresponding to choice
